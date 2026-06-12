ceeb96703bd1950c0cff3799be7c2a3d47093ca6
Idris Zainal Abidin <idrisz.my@gmail.com>
2026-06-12 12:51:20 +0800

    Add RobotControl namespace with add function

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-	
-})
+/**
+ * Robot Control
+ */
+//% color=#0fbc11 icon="\uf1b9"
+//% block="Robot Control"
+namespace RobotControl {
+
+    /**
+     * Add two numbers
+     */
+    //% block="add %a and %b"
+    export function add(a: number, b: number): number {
+        return a + b
+    }
+
+}
