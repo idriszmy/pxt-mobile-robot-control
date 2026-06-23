cb48fb9309ae8af1be72e98b9983f2ef70dc6e00
Idris Zainal Abidin <idris@Idriss-MacBook-Air.local>
2026-06-23 13:12:14 +0800

    Prepare extension listing metadata

--- test.ts
+++ test.ts
@@ -1,1 +1,20 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+// Compile smoke tests for the public API.
+// This file is not compiled when the package is used as an extension.
+
+if (false) {
+    RobotControl.enterCalibration(DigitalPin.P9)
+    RobotControl.exitCalibration(DigitalPin.P9)
+
+    RobotControl.setPidPowerDiff(500, 0.8, 0.7, 0)
+    RobotControl.pidPowerDiff(512)
+
+    RobotControl.setMotor(MotionBitMotorChannel.M4, MotionBitMotorChannel.M2)
+    RobotControl.robotCalibration(DigitalPin.P9, 170)
+    RobotControl.robotNavigate(RobotDirection.Forward, 170, 100)
+    RobotControl.robotLineFollow(AnalogReadWritePin.P0, 220, true, 0)
+    RobotControl.robotTurnToLine(TurnDirection.Left, 170, AnalogReadWritePin.P0)
+    RobotControl.robotStop()
+
+    RobotControl.calibrateGripperClose(GripperLeftArm.S8, 0, GripperRightArm.S4, 0)
+    RobotControl.gripper(GripperPosition.Toggle)
+}
